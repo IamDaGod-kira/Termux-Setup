3f9e84e3aa767e44400f27f67fe4c9450ca7ba15
Pritam2958 <88766846+Pritam2958@users.noreply.github.com>
2022-09-01 17:04:19 +0530

    Update termuxBanner.sh

--- termuxBanner.sh
+++ termuxBanner.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/remo7777/T-Header
 cd T-Header
 chmod +x t-header.sh
-echo("now an interface will ask a profile name for your termux ...")
+echo("[*] now an interface will ask a profile name for your termux ...")
 ./t-header.sh
 login
