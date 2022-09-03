3bf94a7b74b1b663f5099ec9aef38278b5137b60
Pritam2958 <88766846+Pritam2958@users.noreply.github.com>
2022-09-03 05:40:52 +0530

    Update hackerTools.sh

--- hackerTools.sh
+++ hackerTools.sh
@@ -1,4 +1,4 @@
 pip3 install toolx
 pip3 install tbomb
 git clone https://github.com/htr-tech/nexphisher.git
-echo("[*] Type tbomb to start T-Bomb & toolx to start Tool-x")
+echo "[*] Type tbomb to start T-Bomb & toolx to start Tool-x"
