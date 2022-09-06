2ec3ea962ad094f182e78b581f4c58efe66ac528
Pritam2958 <88766846+Pritam2958@users.noreply.github.com>
2022-09-06 17:03:31 +0530

    update termuxDebian.sh

--- termuxDebian.sh
+++ termuxDebian.sh
@@ -1,4 +1,5 @@
 apt uodate && apt upgrade -y
-pkg install proot-distro
-proot-distro install debian
-echo -e "\e[32m Now to login to debian type [proot-distro login debian] & to logoit from debian type [logout] . Debian is Automatically started now"
+pkg install proot-distro -y
+proot-distro install debian -y
+echo -e "\e[32mNow to login to debian type [proot-distro login debian] & to logout from debian type [logout] . Debian is Automatically started now"
+proot-distro login debian
