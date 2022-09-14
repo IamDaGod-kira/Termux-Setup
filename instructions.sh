54652dbdcf42c74c8981d80ff7dde27dca908688
Pritam2958 <88766846+Pritam2958@users.noreply.github.com>
2022-09-14 16:01:13 +0530

    update instructions.sh

--- instructions.sh
+++ instructions.sh
@@ -1,1 +1,11 @@
-
+echo "Now to use any file, type cd Termux-Setup"
+echo "then type bash <filename>"
+echo "Files and functions present:"
+echo "[*] Basic Setup: bash basicSetup.sh"
+echo "[*] Termux Header: bash termuxBanner.sh"
+echo "[*] Termux OS: bash termuxOS.sh"
+echo "[*] Hacking tools: bash hackerTools"
+echo "[*] Debian in Termux: bash termuxDebian.sh"
+echo "[*] Nethunter in Termux: bash termuxNethunter.sh"
+echo "[*] Ubuntu in Termux: bash termuxUbuntu.sh"
+echo "[*] Styling Addon: bash termuxStyling.sh"
