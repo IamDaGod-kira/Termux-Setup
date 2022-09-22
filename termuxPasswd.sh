cf7eaa34ca34df3e8c635c072b3d9b4de1397e6f
Pritam2958 <88766846+Pritam2958@users.noreply.github.com>
2022-09-22 20:42:28 +0530

    update termuxPasswd.sh

--- termuxPasswd.sh
+++ termuxPasswd.sh
@@ -1,1 +1,8 @@
-
+cd $
+pkg install git
+git clone https://github.com/Ublimjo/Termux-login
+cd Termux-login
+echo "now u will have to input a password u like. remember this password as it will be needed everytime u open termux."
+bash setup.sh
+echo "to login after opening termux type your password and to logout type [logout]"
+echo "to uninstall this function use the file named [termuxPasswd_nope.sh]"
