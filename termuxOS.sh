51677176033b72794293eef09696adac785a2a34
Pritam2958 <88766846+Pritam2958@users.noreply.github.com>
2022-09-22 21:03:30 +0530

    update termuxOS.sh

--- termuxOS.sh
+++ termuxOS.sh
@@ -1,4 +1,1 @@
-echo -e "Now interface will be run to install your OS"
-echo -e "Now you will find a number of os names and their alias . to install one , type {proot-distro install <alias>}"
-proot-distro list
-echo -e "Now type the alias of the OS you want & enter"
+echo -e "Now interface will be run to install your OS"echo -e "Now you will find a number of os names and their alias . to install one , type {proot-distro install <alias>}"proot-distro listecho -e "Now type the alias of the OS you want & enter"
